aa549599ae24622ae612e7adbbeebb19a18a203c
Bovic101 <Victorbolanle72@gmail.com>
2024-08-01 20:34:23 +0200

    b

--- main.c
+++ main.c
@@ -3,11 +3,12 @@ int ft_strcmp(char *s1, char *s2);
 
 int main() 
 {
-    char str1 []= "Hello";
-    char str2 []= "World";
+    char str1 []= "Hello my boy";
+    char str2 []= "World is ok";
 
     int result = ft_strcmp(str1, str2);
-    printf("Comparing '%s' and '%s': %d\n", str1, str2, result);
+    printf("Comparing '%s' and '%s'", str1, str2);
+    printf("\n");
 
     return 0;
 }
